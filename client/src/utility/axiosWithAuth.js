b1ec3af5e39eb8a375655f42347f5f000a7b1ff7
abrahambelaygelaw <abrahambelaygelaw@gmail.com>
2023-12-19 15:30:03 +0300

    redirecting error

--- client/src/utility/axiosWithAuth.js
+++ client/src/utility/axiosWithAuth.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosWithAuth = axios.create({
-  baseURL: "https://pharmacy-wvj8.onrender.com/",
+  baseURL: import.meta.env.VITE_BASE_URL,
   withCredentials: true,
 });
 
