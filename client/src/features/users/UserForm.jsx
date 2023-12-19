02cb2333fe90d52896b908880dcbbdb348cf4ecf
abrahambelaygelaw <abrahambelaygelaw@gmail.com>
2023-12-20 00:45:04 +0300

    fixing users

--- client/src/features/users/UserForm.jsx
+++ client/src/features/users/UserForm.jsx
@@ -203,7 +203,7 @@ const UserForm = () => {
               <button
                 disabled={loading}
                 type="submit"
-                class="flex justify-center items-center text-white bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
+                class="flex justify-center items-center text-white bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800 disabled:cursor-not-allowed"
               >
                 {" "}
                 <ClipLoader
